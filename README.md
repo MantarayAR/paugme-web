fa41a7b9a86e194c9f8553ccc7b2ff961f0ed6dd
Ivan Montiel <idmontie@asu.edu>
2015-07-23 23:14:01 -0700

    Initial laravel install and home page

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # paugme-web
 Website for Paugme
+
+# Developing
+
+You can use `php artisan serve`. It is recommended that you use Vagrant though.
+
+```
+npm install --global gulp
+npm install
+```
---
+++ paugme/resources/views/welcome.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700,400|Roboto:100' rel='stylesheet' type='text/css'>
+        <link href="css/app.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+
+        <div class="home">
+            <div class="home__welcome">
+                <div class="home__welcome_card"></div>
+                <div class="home__welcome_panel">
+                    <div class="todo">Icon</div>
+
+                    <h1>Paugme Packs</h1>
+
+                    <h2>Educational kits for children</h2>
+
+                    <h3>Learn. Play. Fun.</h3>
+
+                    <a href="sign-up" class="button button-large">Sign Up for Early Access</a>
+                </div>
+            </div>
+
+            <div class="home__about">
+                <blockquote>
+                    <p>&ldquo;Wow, this is a great quote.&rdquo;</p>
+
+                    <cite>
+                        <p>- My name should be gray</p>
+                    </cite>
+                </blockquote>
+            </div>
+
+            <div class="home__packages">
+                <p>Paugme Packs deliver new educational kits to you once a month!</p>
+                <p>Get all of the following packs!</p>
+
+                <div class="home__packages_pack">
+                    <div class="row">
+                        <div class="column one-third">
+                            <img class="img-responsive" src="http://placehold.it/200x200"/>
+                        </div>
+                        <div class="column two-thirds">
+                            <h2>Circuits</h2>
+                            <p>
+                                Learn circuits, logic, and how to make
+                                a motion sensor device!
+                            </p>
+                            <p>
+                                Includes:
+                            </p>
+                            <ul>
+                                <li>Activities</li>
+                                <li>Flash Cards</li>
+                                <li>Learning Book</li>
+                                <li>Circuits App</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="home__packages_pack">
+                    <img src="http://placehold.it/200x200"/>
+                    <h2>Biology</h2>
+                    <p>
+                        Discover cells, viruses, and the microscopic world!
+                    </p>
+                    <p>
+                        Includes:
+                    </p>
+                    <ul>
+                        <li>Learning Book</li>
+                        <li>Lab Experiments Book</li>
+                        <li>Cells and Viruses App</li>
+                    </ul>
+                </div>
+                <div class="home__packages_pack">
+                    <img src="http://placehold.it/200x200"/>
+                    <h2>Chemistry</h2>
+                </div>
+                <div class="home__packages_pack">
+                    <img src="http://placehold.it/200x200"/>
+                    <h2>Programming</h2>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
