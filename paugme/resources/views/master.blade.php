a89e3283123b39344757d7038322d9e8eec11058
Ivan Montiel <idmontie@asu.edu>
2015-07-28 18:04:04 -0700

    Add favicons

--- paugme/resources/views/master.blade.php
+++ paugme/resources/views/master.blade.php
@@ -4,17 +4,32 @@
     <title>Paugme Packs</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700,400|Roboto:100' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700,400|Roboto:100' rel='stylesheet'
+          type='text/css'>
     <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="css/app.css" rel="stylesheet" type="text/css">
     @yield('head')
+    @include('_favicon')
 </head>
 <body>
-  @yield('content')
+<header>
 
-  <footer>
+</header>
 
-  </footer>
-  <script src="js/jquery-1.11.3.min.js"></script>
-  @yield('scripts')
+@yield('content')
+
+<footer>
+    <h2>Paugme Packs</h2>
+
+    <p>Copyright &copy; 2015 Mantaray AR LLC</p>
+
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/blog">Blog</a></li>
+
+    </ul>
+</footer>
+<script src="js/jquery-1.11.3.min.js"></script>
+@yield('scripts')
 </body>
