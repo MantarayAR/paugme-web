fbf79dc8a9b330be374b64e83c737656bdf0f6e3
Ivan Montiel <idmontie@asu.edu>
2015-07-29 21:58:30 -0700

    Scaffolding for contacts

---
+++ paugme/app/Handlers/Commands/CreateContactCommandHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Handlers\Commands;
+
+use CreateContactCommand;
+use Illuminate\Queue\InteractsWithQueue;
+
+class CreateContactCommandHandler
+{
+    /**
+     * Create the command handler.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the command.
+     *
+     * @param  CreateContactCommand  $command
+     * @return void
+     */
+    public function handle(CreateContactCommand $command)
+    {
+        //
+    }
+}
---
+++ paugme/app/Handlers/Events/NotifyAdministratorsOfNewContact.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Handlers\Events;
+
+use App\Events\ContactWasCreatedEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NotifyAdministratorsOfNewContact
+{
+    /**
+     * Create the event handler.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ContactWasCreatedEvent  $event
+     * @return void
+     */
+    public function handle(ContactWasCreatedEvent $event)
+    {
+        //
+    }
+}
