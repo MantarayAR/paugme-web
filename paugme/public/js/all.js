2b5a13e34a62b27c84907e3db6c308f8955d7312
Ivan Montiel <idmontie@asu.edu>
2015-08-20 22:02:30 -0700

    Add mobile menu, includes JavaScript changes
    The mobile menu pops out of the left if a user
    is on a mobile view and clicks on the mobile
    menu button.
    
    Close #4.

--- paugme/public/js/all.js
+++ paugme/public/js/all.js
@@ -3,3 +3,47 @@ var headroom  = new Headroom(headroomEl, {
     offset: 50
 });
 headroom.init();
+
+/**
+ *  Mobile Menu
+ */
++function ($) {
+    var OPEN   = 'open';
+    var CLOSED = 'closed';
+
+    var mobileMenuState = CLOSED;
+
+    var mobileMenuSelector       = '[data-is="mobile-menu"]';
+    var mobileMenuButtonSelector = '[data-for="mobile-menu"]';
+
+    function isOpen() {
+        return mobileMenuState === OPEN;
+    }
+
+    function open() {
+        $(mobileMenuSelector).addClass( 'active' );
+        mobileMenuState = OPEN;
+    }
+
+    function close() {
+        $(mobileMenuSelector).removeClass( 'active' );
+        mobileMenuState = CLOSED;
+    }
+
+    $(mobileMenuButtonSelector).click(function (e) {
+        e.stopPropagation();
+        if ( isOpen() ) {
+            close();
+        } else {
+            open();
+        }
+    });
+    $(document).click(function ( e ) {
+        if ( isOpen() ) {
+            if ( $(e.target).closest(mobileMenuSelector).length === 0) {
+                close();
+            }
+        }
+    });
+
+}(jQuery);
